14fa33a6986d6ffc77741e8f7629e335a108312b
Victor BUZZEGOLI <vbuzzegoli@hawk.iit.edu>
2018-10-05 09:09:44 -0400

    4M 1.8 compliance

--- example/spy/middleware/index.js
+++ example/spy/middleware/index.js
@@ -5,8 +5,72 @@ const defaults = {
   onLog = false,
   onInvalidInput = false,
   silentCrash = false,
-  _skip = false // used for backward compatibility with thunk / saga
+  _skip = false // skip the given middleware (action already dispatched once)
 };
+
+const inputTypeNotValid = ({ input, inputName, type, silentCrash, onInvalidInput, middlewareName, defaults, action, next, dispatch, shouldReturn }) => {
+  if (silentCrash) console.log(`[${middlewareName}] ${inputName} is invalid - ${type} expected.`, action);
+  if (onInvalidInput){
+    onInvalidInput(inputName, action, next, dispatch); 
+    shouldReturn = true;
+  } else {
+    input = defaults[inputName];
+  }
+  return [input, shouldReturn];
+}
+
+const validateInput = (input, inputName, type, { silentCrash, onInvalidInput, middlewareName, defaults, action, next, dispatch }) => {
+  let shouldReturn = false;
+  const toProcess = { 
+    input, inputName, type, 
+    silentCrash, onInvalidInput, 
+    middlewareName, defaults, 
+    action, next, dispatch, 
+    shouldReturn };
+
+  switch(type){
+
+    case `boolean`:
+      if (typeof input !== `boolean`){
+        let [ input, shouldReturn ] = inputTypeNotValid(toProcess);
+      }
+      return [input, shouldReturn];
+
+    case `function`:
+      if (typeof input !== `function` && input !== defaults[inputName]){
+        let [ input, shouldReturn ] = inputTypeNotValid(toProcess);
+      }
+      return [input, shouldReturn];
+
+    case `number`:
+      if (typeof input !== `number` && input !== defaults[inputName]){
+        let [ input, shouldReturn ] = inputTypeNotValid(toProcess);
+      }
+      return [input, shouldReturn];
+
+    case `string`:
+      if (typeof input !== `string` && input !== defaults[inputName]){
+        let [ input, shouldReturn ] = inputTypeNotValid(toProcess);
+      }
+      return [input, shouldReturn];
+
+    case `array`:
+      if (!Array.isArray(input) && input !== defaults[inputName]){
+        let [ input, shouldReturn ] = inputTypeNotValid(toProcess);
+      }
+      return [input, shouldReturn];
+
+    case `object`:
+      if (!(typeof input == `object` && input instanceof Object && !(input instanceof Array)) && input !== defaults[inputName]){
+        let [ input, shouldReturn ] = inputTypeNotValid(toProcess);
+      }
+      return [input, shouldReturn];
+
+    default: 
+      return inputTypeNotValid(toProcess);
+
+  }
+}
 
 const spy = store => next => action => {
   // Omit middleware if no related config in the action
@@ -28,8 +92,12 @@ const spy = store => next => action => {
     if (typeof onInvalidInput !== "function" && onInvalidInput !== defaults.onInvalidInput) {
       silentCrash = defaults.silentCrash;
     } else {
-      onInvalidInput(`silentCrash`, action, next, dispatch);
-      return;
+      if (onInvalidInput) {
+        onInvalidInput(`silentCrash`, action, next, dispatch);
+        return;
+      } else {
+        silentCrash = defaults.silentCrash;
+      }
     }
   }
 
@@ -38,35 +106,17 @@ const spy = store => next => action => {
     onInvalidInput = defaults.onInvalidInput;
   }
 
-  if (typeof log !== "boolean"){
-    if (silentCrash) console.log(`[Spy] log is invalid - boolean expected.`, action);
-    if (onInvalidInput){
-      onInvalidInput(`log`, action, next, dispatch);
-      return;
-    } else {
-      log = defaults.log;
-    }
-  }
+  const validationMeta = { silentCrash, onInvalidInput, `Spy`, defaults, action, next, dispatch };
 
-  if (typeof onLog !== "function" && onLog !== defaults.onLog){
-    if (silentCrash) console.log(`[Spy] onLog is invalid - function expected.`, action);
-    if (onInvalidInput){
-      onInvalidInput(`onLog`, action, next, dispatch);
-      return;
-    } else {
-      onLog = defaults.onLog;
-    }
-  }
+  let [ log, shouldReturn ] = validateInput(log, `log`, `boolean`, validationMeta);
+  if (shouldReturn) return;
 
-  if (typeof _skip !== "boolean"){
-    if (silentCrash) console.log(`[Spy] _skip is invalid - boolean expected.`, action);
-    if (onInvalidInput){
-      onInvalidInput(`_skip`, action, next, dispatch);
-      return;
-    } else {
-      _skip = defaults._skip;
-    }
-  }
+  let [ onLog, shouldReturn ] = validateInput(onLog, `onLog`, `function`, validationMeta);
+  if (shouldReturn) return;
+
+  let [ _skip, shouldReturn ] = validateInput(_skip, `_skip`, `boolean`, validationMeta);
+  if (shouldReturn) return;
+
   
   // Loop Protection
   if (_skip) {
